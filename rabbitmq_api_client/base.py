63837493c593a559188319d9989c02e44f63c251
elie <contact@elie-terrien.fr>
2023-11-27 22:19:56 +0100

    add docstrings + version httpx

--- rabbitmq_api_client/base.py
+++ rabbitmq_api_client/base.py
@@ -4,7 +4,36 @@ from rabbitmq_api_client.exceptions import RabbitMQAPIError
 
 
 class BaseClient:
+	"""Base client for making HTTP requests with basic authentication.
+
+	Args:
+		base_url (str): The base URL of the API.
+		username (str): The username for basic authentication.
+		password (str): The password for basic authentication.
+
+	Attributes:
+		base_url (str): The base URL of the API.
+		username (str): The username for basic authentication.
+		password (str): The password for basic authentication.
+		client (httpx.Client): The HTTP client for making requests.
+
+	Methods:
+		request: Make a generic HTTP request.
+		get: Make a GET request.
+		post: Make a POST request.
+		put: Make a PUT request.
+		delete: Make a DELETE request.
+		close: Close the HTTP client.
+		__del__: Destructor to automatically close the client on object deletion.
+		__repr__: String representation of the BaseClient instance.
+
+	Example:
+		client = BaseClient("https://api.example.com", "user", "pass")
+		response = client.get("/resource")
+		print(response)
+	"""
 	def __init__(self, base_url: str, username: str, password: str):
+		"""Initialize the BaseClient with the provided base URL, username, and password."""
 		self.base_url = base_url
 		self.username = username
 		self.password = password
@@ -13,6 +42,18 @@ class BaseClient:
 		)
 
 	def request(self, method: str, url: str, **kwargs):
+		"""Make a generic HTTP request.
+		Args:
+			method (str): The HTTP method (e.g., 'get', 'post', 'put', 'delete').
+			url (str): The URL for the request.
+			**kwargs: Additional keyword arguments to pass to the request.
+
+		Returns:
+			dict or str: The JSON response if available, or the raw text response.
+
+		Raises:
+			RabbitMQAPIError: If the response status code is not in the 2xx range.
+		"""
 		response = self.client.request(method, url, **kwargs)
 		if not (200 <= response.status_code < 300):
 			raise RabbitMQAPIError(response, response.status_code, response.text)
@@ -25,22 +66,58 @@ class BaseClient:
 			return {}
 
 	def get(self, url: str, params: dict = None):
+		"""Make a GET request.
+		Args:
+			url (str): The URL for the GET request.
+			params (dict, optional): Additional parameters for the GET request.
+
+		Returns:
+			dict or str: The JSON response if available, or the raw text response.
+		"""
 		return self.request('get', url, params=params)
 
 	def post(self, url: str, data: dict = None):
+		"""Make a POST request.
+		Args:
+			url (str): The URL for the POST request.
+			data (dict, optional): The JSON data to include in the POST request.
+
+		Returns:
+			dict or str: The JSON response if available, or the raw text response.
+		"""
 		return self.request('post', url, json=data)
 
 	def put(self, url: str, data: dict = None):
+		"""Make a PUT request.
+
+		Args:
+			url (str): The URL for the PUT request.
+			data (dict, optional): The JSON data to include in the PUT request.
+
+		Returns:
+			dict or str: The JSON response if available, or the raw text response.
+		"""
 		return self.request('put', url, json=data)
 
 	def delete(self, url: str):
+		"""Make a DELETE request.
+
+		Args:
+			url (str): The URL for the DELETE request.
+
+		Returns:
+			dict or str: The JSON response if available, or the raw text response.
+		"""
 		return self.request('delete', url)
 
 	def close(self):
+		"""Close the HTTP client."""
 		self.client.close()
 
 	def __del__(self):
+		"""Destructor to automatically close the client on object deletion."""
 		self.close()
 
 	def __repr__(self):
+		"""String representation of the BaseClient instance."""
 		return f'<{self.__class__.__name__} {self.base_url}>'
--- rabbitmq_api_client/client.py
+++ rabbitmq_api_client/client.py
@@ -5,42 +5,56 @@ from rabbitmq_api_client.schemas import CreateQueue, CreateUser, CreateVhost, Pe
 
 
 class RabbitMQClient(BaseClient):
+	"""
+	RabbitMQ client for interacting with the RabbitMQ server API.
+
+	Args:
+		base_url (str): The base URL of the RabbitMQ server.
+		username (str): The username for authentication.
+		password (str): The password for authentication.
+	"""
 	def __init__(self, base_url: str, username: str, password: str):
 		"""Initialize a RabbitMQ client.
 
-		:param base_url: the base url of the RabbitMQ server
-		:param username: the username to use for authentication
-		:param password: the password to use for authentication
+		Args:
+			base_url (str): The base URL of the RabbitMQ server.
+			username (str): The username to use for authentication.
+			password (str): The password to use for authentication.
 		"""
-
 		super().__init__(base_url, username, password)
 
 	def get_overview(self) -> dict:
 		"""Get an overview of the RabbitMQ server.
 
-		:return: dict of overview information
+		Returns:
+			dict: Overview information.
 		"""
 		return self.get('/api/overview')
 
 	def get_cluster_name(self) -> dict:
 		"""Get the cluster name of the RabbitMQ server.
 
-		:return: dict with cluster name
+		Returns:
+			dict: Cluster name information.
 		"""
 		return self.get('/api/cluster-name')
 
 	def get_vhosts(self) -> list[dict]:
 		"""Get all vhosts on the RabbitMQ server.
 
-		:return: a list of vhosts
+		Returns:
+			list[dict]: A list of vhosts.
 		"""
 		return self.get('/api/vhosts')
 
 	def get_vhost(self, vhost: str) -> dict:
 		"""Get a vhost on the RabbitMQ server.
 
-		:param name: the name of the vhost
-		:return: dict of vhost
+		Args:
+			vhost (str): The name of the vhost.
+
+		Returns:
+			dict: Information about the specified vhost.
 		"""
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.get(f'/api/vhosts/{vhost}')
@@ -48,8 +62,11 @@ class RabbitMQClient(BaseClient):
 	def create_vhost(self, vhost: CreateVhost) -> dict:
 		"""Create a new vhost on the RabbitMQ server.
 
-		:param vhost: pydantic model of vhost
-		:return: empty dict
+		Args:
+			vhost (CreateVhost): Pydantic model representing the vhost.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		vhost_dict = vhost.model_dump(exclude_unset=True)
 		vhost_name = urllib.parse.quote(vhost_dict.pop('name'), safe='')
@@ -58,8 +75,11 @@ class RabbitMQClient(BaseClient):
 	def delete_vhost(self, vhost: str) -> dict:
 		"""Delete a vhost on the RabbitMQ server.
 
-		:param vhost: name of vhost
-		:return: empty dict
+		Args:
+			vhost (str): Name of the vhost.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.delete(f'/api/vhosts/{vhost}')
@@ -67,15 +87,19 @@ class RabbitMQClient(BaseClient):
 	def get_queues(self) -> list[dict]:
 		"""Get all queues on the RabbitMQ server.
 
-		:return: a list of queues
+		Returns:
+			list[dict]: A list of queues.
 		"""
 		return self.get('/api/queues')
 
 	def get_vhost_queues(self, vhost: str) -> list[dict]:
 		"""Get all queues for a specific vhost on the RabbitMQ server.
 
-		:param vhost: name of vhost
-		:return: list of queues
+		Args:
+			vhost (str): Name of the vhost.
+
+		Returns:
+			list[dict]: List of queues for the specified vhost.
 		"""
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.get(f'/api/queues/{vhost}')
@@ -83,9 +107,12 @@ class RabbitMQClient(BaseClient):
 	def create_queue(self, vhost: str, queue: CreateQueue) -> dict:
 		"""Create a new queue on a specific vhost on the RabbitMQ server.
 
-		:param vhost: name of vhost
-		:param queue: name of queue
-		:return: empty dict
+		Args:
+			vhost (str): Name of the vhost.
+			queue (CreateQueue): Pydantic model representing the queue.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		queue_dict = queue.model_dump(exclude_unset=True)
 		queue_name = urllib.parse.quote(queue_dict.pop('name'), safe='')
@@ -93,12 +120,14 @@ class RabbitMQClient(BaseClient):
 		return self.put(f'/api/queues/{vhost}/{queue_name}', queue_dict)
 
 	def get_vhost_queue(self, vhost: str, queue_name: str) -> dict:
-		"""
-		Get a queue on a specific vhost on the RabbitMQ server.
+		"""Get a queue on a specific vhost on the RabbitMQ server.
 
-		:param vhost: name of vhost
-		:param queue_name: name of queue
-		:return: dict of queue
+		Args:
+			vhost (str): Name of the vhost.
+			queue_name (str): Name of the queue.
+
+		Returns:
+			dict: Information about the specified queue.
 		"""
 		queue_name = urllib.parse.quote(queue_name, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
@@ -107,23 +136,30 @@ class RabbitMQClient(BaseClient):
 	def get_users(self) -> list:
 		"""Get all users on the RabbitMQ server.
 
-		:return: list of users
+		Returns:
+			list: List of users.
 		"""
 		return self.get('/api/users')
 
 	def get_user(self, username: str) -> dict:
 		"""Get a user on the RabbitMQ server.
 
-		:param username: name of user
-		:return: dict of user
+		Args:
+			username (str): Name of the user.
+
+		Returns:
+			dict: Information about the specified user.
 		"""
 		return self.get(f'/api/users/{username}')
 
 	def create_user(self, user: CreateUser) -> dict:
 		"""Create a new user on the RabbitMQ server.
 
-		:param user:
-		:return: empty dict
+		Args:
+			user (CreateUser): Pydantic model representing the user.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		return self.put(
 			f'/api/users/{user.name}', {'password': user.password, 'tags': user.tags}
@@ -132,118 +168,200 @@ class RabbitMQClient(BaseClient):
 	def delete_user(self, username: str) -> dict:
 		"""Delete a user on the RabbitMQ server.
 
-		:param username: name of user
-		:return: empty dict
+		Args:
+			username (str): Name of the user.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		return self.delete(f'/api/users/{username}')
 
 	def get_user_permissions(self, username: str) -> list[dict]:
-		"""
-		Get the permissions of a user on the RabbitMQ server.
-		:param username: the name of the user
-		:return: list of permissions
+		"""Get the permissions of a user on the RabbitMQ server.
+
+		Args:
+			username (str): Name of the user.
+
+		Returns:
+			list[dict]: List of permissions for the specified user.
 		"""
 		return self.get(f'/api/users/{username}/permissions')
 
 	def get_user_topic_permissions(self, username: str) -> list[dict]:
+		"""Get the topic permissions of a user on the RabbitMQ server.
+
+		Args:
+			username (str): Name of the user.
+
+		Returns:
+			list[dict]: List of topic permissions for the specified user.
+		"""
 		return self.get(f'/api/users/{username}/topic-permissions')
 
 	def get_users_without_permissions(self) -> list[dict]:
+		"""Get all users without permissions on the RabbitMQ server.
+
+		Returns:
+			list[dict]: List of users without permissions.
+		"""
 		return self.get('/api/users-without-permissions')
 
 	def get_permissions(self) -> list[dict]:
+		"""Get all permissions on the RabbitMQ server.
+
+		Returns:
+			list[dict]: List of permissions.
+		"""
 		return self.get('/api/permissions')
 
 	def get_user_permissions_on_vhost(self, username: str, vhost: str) -> dict:
+		"""Get the permissions of a user on a specific vhost on the RabbitMQ server.
+
+		Args:
+			username (str): Name of the user.
+			vhost (str): Name of the vhost.
+
+		Returns:
+			dict: Permissions for the specified user on the specified vhost.
+		"""
 		username = urllib.parse.quote(username, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.get(f'/api/permissions/{vhost}/{username}')
 
 	def create_user_permissions_on_vhost(self, username: str, vhost: str, permissions: Permissions) -> dict:
+		"""Create permissions for a user on a specific vhost on the RabbitMQ server.
+
+		Args:
+			username (str): Name of the user.
+			vhost (str): Name of the vhost.
+			permissions (Permissions): Pydantic model representing the permissions.
+
+		Returns:
+			dict: Empty dictionary.
+		"""
 		username = urllib.parse.quote(username, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.put(f'/api/permissions/{vhost}/{username}', permissions.model_dump())
 
 	def delete_user_permissions_on_vhost(self, username: str, vhost: str) -> dict:
+		"""Delete permissions for a user on a specific vhost on the RabbitMQ server.
+		Args:
+			username (str): Name of the user.
+			vhost (str): Name of the vhost.
+
+		Returns:
+			dict: Empty dictionary.
+		"""
 		username = urllib.parse.quote(username, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.delete(f'/api/permissions/{vhost}/{username}')
 
 	def get_exchanges(self) -> list[dict]:
-		"""
-		Get all exchanges on the RabbitMQ server.
-		:return: list of exchanges
+		"""Get all exchanges on the RabbitMQ server.
+		Returns:
+			list[dict]: A list of exchanges.
 		"""
 		return self.get('/api/exchanges')
 
 	def get_vhost_exchanges(self, vhost: str) -> list[dict]:
-		"""
-		Get all exchanges on a specific vhost on the RabbitMQ server.
-		:param vhost: name of vhost
-		:return: a list of exchanges
+		"""Get all exchanges for a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+
+		Returns:
+			list[dict]: List of exchanges for the specified vhost.
 		"""
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.get(f'/api/exchanges/{vhost}')
 
 	def get_vhost_exchange(self, vhost: str, exchange_name: str) -> dict:
-		"""
-		Get an exchange on a specific vhost on the RabbitMQ server.
-		:param vhost: name of vhost
-		:param exchange_name: name of exchange
-		:return: dict of exchange
+		"""Get an exchange on a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+			exchange_name (str): Name of the exchange.
+
+		Returns:
+			dict: Information about the specified exchange.
 		"""
 		exchange_name = urllib.parse.quote(exchange_name, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.get(f'/api/exchanges/{vhost}/{exchange_name}')
 
 	def create_exchange(self, vhost: str, exchange_name: str, exchange: Exchange) -> dict:
-		"""
-		Create an exchange on a specific vhost on the RabbitMQ server.
-		:param vhost: name of vhost
-		:param exchange_name: name of exchange
-		:param exchange: body of exchange
-		:return: empty dict
+		"""Create a new exchange on a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+			exchange_name (str): Name of the exchange.
+			exchange (Exchange): Pydantic model representing the exchange.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		exchange_name = urllib.parse.quote(exchange_name, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.put(f'/api/exchanges/{vhost}/{exchange_name}', exchange.model_dump())
 
 	def delete_exchange(self, vhost: str, exchange_name: str) -> dict:
-		"""
-		Delete an exchange on a specific vhost on the RabbitMQ server.
-		:param vhost: name of vhost
-		:param exchange_name: name of exchange
-		:return: empty dict
+		"""Delete an exchange on a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+			exchange_name (str): Name of the exchange.
+
+		Returns:
+			dict: Empty dictionary.
 		"""
 		exchange_name = urllib.parse.quote(exchange_name, safe='')
 		vhost = urllib.parse.quote(vhost, safe='')
 		return self.delete(f'/api/exchanges/{vhost}/{exchange_name}')
 
 	def get_bindings(self) -> list[dict]:
-		"""
-		Get the list of all bindings
-		:return:
+		"""Get all bindings on the RabbitMQ server.
+
+		Returns:
+			list[dict]: A list of bindings.
 		"""
 		return self.get('/api/bindings')
 
 	def get_vhost_bindings(self, vhost: str) -> list[dict]:
-		"""
-		Get the list of all bindings for a vhost
-		:param vhost: the name of vhost
-		:return: list of dict
+		"""Get all bindings for a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+
+		Returns:
+			list[dict]: List of bindings for the specified vhost.
 		"""
 		return self.get(f'/api/bindings/{vhost}')
 
-	def get_vhost_binding(self, vhost, exchange, queue: str):
-		"""
-		:param vhost:
-		:param exchange:
-		:param queue:
-		:return:
+	def get_vhost_binding(self, vhost: str, exchange: str, queue: str) -> dict:
+		"""Get a binding for a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+			exchange (str): Name of the exchange.
+			queue (str): Name of the queue.
+
+		Returns:
+			dict: Information about the specified binding.
 		"""
 		return self.get(f'/api/bindings/{vhost}/e/{exchange}/q/{queue}')
 
 	def create_vhost_binding(self, vhost: str, exchange: str, queue: str, binding: Binding):
+		"""Create a new binding for a specific vhost on the RabbitMQ server.
+
+		Args:
+			vhost (str): Name of the vhost.
+			exchange (str): Name of the exchange.
+			queue (str): Name of the queue.
+			binding (Binding): Pydantic model representing the binding.
+
+		Returns:
+			dict: Empty dictionary.
+		"""
 		return self.post(f'/api/bindings/{vhost}/e/{exchange}/q/{queue}', binding.model_dump())
 
 
